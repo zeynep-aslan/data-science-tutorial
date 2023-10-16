0fd40d40c2516788118933dab0aa11f9b8c9292f
zeynep-aslan <zeynep.aslan13@hotmail.com>
2023-10-16 14:46:22 +0300

    initial commit

---
+++ python-programming/setMethods.py
@@ -0,0 +1,20 @@
+#Veri Yapilari - Setler
+
+#Set olusturmak
+
+# sirasizdirler -> belli bir siradaki elemana gidemezsin
+# eşsizdirler(tekrar eden degerlerden olusamaz)
+# degistirilebilir
+# farkli tipleri barindirabilirler
+
+# performans odadkli, daha cok hiz istedigimizde
+# icerisinde çoklama durumunu beklemedigimiz unique(essiz) degerler barindiran
+# matematikteki kumelere benzer
+
+ali = "lutfen_ata_bak ma_uza ya_git"
+s = set(ali)
+s  #her elemandan(harften) bir tane var
+
+# Eleman ekleme & cikarma
+
+
